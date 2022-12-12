959a97922ae2e43b0d2b12561bf72a6e8abe627f
deepak pundir <sendit2deepak@gmail.com>
2022-12-12 11:41:51 +0530

    replaced metadeta and public images

--- apps/web/src/config/constants/meta.ts
+++ apps/web/src/config/constants/meta.ts
@@ -3,10 +3,9 @@ import { ContextApi } from '@pancakeswap/localization'
 import { PageMeta } from './types'
 
 export const DEFAULT_META: PageMeta = {
-  title: 'MMO Swap',
-  description:
-    'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
-  image: 'https://pancakeswap.finance/images/hero.png',
+  title: 'MMOSWAP',
+  description: 'The most popular AMM on Polygon chain!.',
+  image: '',
 }
 
 interface PathList {
